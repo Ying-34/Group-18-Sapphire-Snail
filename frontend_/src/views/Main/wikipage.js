ef5c27f75f3ca992a946eeefba9341fb032ed992
lli708 <80568642+lli708@users.noreply.github.com>
2021-05-06 03:06:58 +1200

    Add files via upload
    wikipage

---
+++ frontend_/src/views/Main/wikipage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Article from './article';
+import Nav from '../../components/Nav/Nav'
+
+const wikipage = () => {
+    return (
+        <div>
+          <Nav />
+          <Article />
+        </div>
+      )
+    }
+
+    export default wikipage
