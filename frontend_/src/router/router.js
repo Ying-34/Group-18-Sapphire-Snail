af2701dee537fe61d35bc61f0ea6f0eb58b020f0
Ying <yingwork34@gmail.com>
2021-05-06 01:56:27 +1200

    push to main

--- frontend_/src/router/router.js
+++ frontend_/src/router/router.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2021-05-05 02:29:45
+ * @LastEditTime: 2021-05-06 01:47:30
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /Group-18-Sapphire-Snail/frontend_/src/router/router.js
+ */
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Home from '../views/Home/Home'
 import Login from '../views/Login/Login'
@@ -6,7 +14,7 @@ import AboutUs from '../views/AboutUs/AboutUs'
 import ContactUs from '../views/ContactUs/ContactUs'
 import QuestionAndAnswer from '../views/QuestionAndAnswer/QuestionAndAnswer'
 
-// here are router assembly
+// router assembly
 const Router = () => {
   return (
     <BrowserRouter>
