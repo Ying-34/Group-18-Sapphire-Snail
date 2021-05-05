d08d64a86ee9ed5f6dd1c33d12e73d4114645b14
lli708 <80568642+lli708@users.noreply.github.com>
2021-05-06 03:35:09 +1200

    Add files via upload
    index

--- frontend_/src/App.js
+++ frontend_/src/App.js
@@ -4,6 +4,10 @@ import './assets/css/common.css'
 import {BrowserRouter, Route, Switch } from 'react-router-dom'
 import wikipage from './views/Main/wikipage'
 import Home from '../src/views/Home/Home'
+import Login from '../src/views/Login/Login'
+import AboutUs from '../src/views/AboutUs/AboutUs'
+import ContactUs from '../src/views/ContactUs/ContactUs'
+import QuestionAndAnswer from '../src/views/QuestionAndAnswer/QuestionAndAnswer'
 
 const App =() =>{
   return (
@@ -12,6 +16,10 @@ const App =() =>{
         <Switch>
         <Route path="/home/" component={Home}/>
         <Route path="/wikipage/" component={wikipage}/>
+        <Route path="/Login/" component={Login}/>
+        <Route path="/About-us/" component={AboutUs}/>
+        <Route path="/Contact-us/" component={ContactUs}/>
+        <Route path="/question-answer/" component={QuestionAndAnswer}/>
         </Switch>
       </BrowserRouter>
     </div>
