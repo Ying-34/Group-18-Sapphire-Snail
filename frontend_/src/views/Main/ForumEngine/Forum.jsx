777e110e410da0f1cc1aa8b7a280ae238db3e3cf
lli708 <80568642+lli708@users.noreply.github.com>
2021-05-07 02:14:21 +1200

    Add files via upload
    comment engine done

---
+++ frontend_/src/views/Main/ForumEngine/Forum.jsx
@@ -0,0 +1,19 @@
+import { Divider } from 'antd';
+import React from 'react';
+import AddComment from './AddComment'
+//import ReactDOM from  'react-dom';
+import './Forum.css';
+
+const Forum =()=>{
+    localStorage.setItem('commentIndex', 1);
+
+    return(
+          <div id='divForum' className="App">
+              <Divider style={{ borderWidth: 3, borderColor: 'skyblue'}} />
+              <h1>Comment</h1>
+              <AddComment/>
+          </div>
+    );
+}
+
+export default Forum;
