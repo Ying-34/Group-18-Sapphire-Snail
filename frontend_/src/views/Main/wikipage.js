9c522fb5d5583457c2740ce5a280ab5be23de4a0
lli708 <80568642+lli708@users.noreply.github.com>
2021-05-07 02:29:15 +1200

    update wikipage comment

--- frontend_/src/views/Main/wikipage.js
+++ frontend_/src/views/Main/wikipage.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Article from './article';
 import Nav from '../../components/Nav/Nav'
+import Forum from '../Main/ForumEngine/Forum.jsx'
 
 const wikipage = () => {
     return (
         <div>
           <Nav />
           <Article />
+          <Forum />
         </div>
       )
     }
