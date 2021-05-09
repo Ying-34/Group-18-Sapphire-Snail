80e539b57383daabcbeddf5a37c82af8c9ace4f0
lli708 <80568642+lli708@users.noreply.github.com>
2021-05-09 23:00:26 +1200

    final commit

--- backend_/routes/noteRouter.js
+++ backend_/routes/noteRouter.js
@@ -20,15 +20,39 @@ router.route('/postTip').post((req,res) =>{
     });
 
     newNote.save()
-        .then(() => res.json('note added!'))
-      .catch(err => res.status(400).json('Error: ' + err));
+        .then(() => res.json(newNote.id))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/updateTipContent/:id').post((req,res) =>{
+    notes.findById(req.params.id)
+        .then(note =>{
+            note.content = req.body.content;
+            note.save()
+                .then(() => res.json('note updated!'))
+                .catch(err => res.status(400).json('Error: ' + err));
+        })
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/updateTipTit/:id').post((req,res) =>{
+    notes.findById(req.params.id)
+        .then(note =>{
+            note.noteTit = req.body.noteTit;
+            note.save()
+                .then(() => res.json('note updated!'))
+                .catch(err => res.status(400).json('Error: ' + err));
+        })
+        .catch(err => res.status(400).json('Error: ' + err));
 });
 
 router.route('/get/:pgName/:userName').get((req,res) =>{
-    notes.find({pageName: req.body.pageName})
+    //console.log(req.params);
+    notes.find({pageName: req.params.pgName, userName: req.params.userName})
     .then(allNotes =>{
         res.json(allNotes);
     })
     .catch(err => res.status(400).json('Error: ' + err));
 });
+
 module.exports = router;
